2a5de3845d042034bda37ea308e339966c9af45a
MOHAMAD MIRZA <19025376@myrp.edu.sg>
2021-05-18 15:45:10 +0800

    a

--- app/src/main/java/com/myapplicationdev/android/p05_ndpsongs/ModifyActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p05_ndpsongs/ModifyActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
@@ -47,7 +48,11 @@ public class ModifyActivity extends AppCompatActivity {
 
         });
 
-        btnCancel.setOnClickListener(v -> {
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
 
         });
     }

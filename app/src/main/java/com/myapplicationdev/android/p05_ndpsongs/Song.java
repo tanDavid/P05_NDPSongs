7d1d7b068eefbfab369a09f0acc0ec19d6e4f8c3
MOHAMAD MIRZA <19025376@myrp.edu.sg>
2021-05-18 13:31:51 +0800

    1

---
+++ app/src/main/java/com/myapplicationdev/android/p05_ndpsongs/Song.java
@@ -0,0 +1,59 @@
+package com.myapplicationdev.android.p05_ndpsongs;
+
+import java.io.Serializable;
+
+public class Song implements Serializable {
+    private int id;
+    private String title;
+    private String singers;
+    private int year;
+    private int stars;
+
+    public Song(int id, String title, String singers, int year, int stars) {
+        this.title = title;
+        this.singers = singers;
+        this.year = year;
+        this.stars = stars;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSingers() {
+        return singers;
+    }
+
+    public void setSingers(String singers) {
+        this.singers = singers;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+}

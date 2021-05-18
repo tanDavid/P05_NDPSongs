4125a6755863ae14413f981d6584de7e54ca5231
19002765 <19002765@myrp.edu.sg>
2021-05-18 14:58:10 +0800

    DBHelper and MainActivity created

--- app/src/main/java/com/myapplicationdev/android/p05_ndpsongs/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p05_ndpsongs/MainActivity.java
@@ -2,13 +2,69 @@ package com.myapplicationdev.android.p05_ndpsongs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etTitle, etSinger, etYear;
+    RadioGroup radioGroup;
+    Button btnInsert, btnShow;
+    RadioButton rdButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etTitle = findViewById(R.id.etSongTitle);
+        etSinger = findViewById(R.id.etSingers);
+        etYear = findViewById(R.id.etYear);
+        radioGroup = findViewById(R.id.radioGrp);
+        btnInsert = findViewById(R.id.btnInsert);
+        btnShow = findViewById(R.id.btnShow);
+
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String title = etTitle.getText().toString();
+                String singer = etSinger.getText().toString();
+                int year = Integer.valueOf(etYear.getText().toString());
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+
+
+                rdButton = findViewById(selectedId);
+                int stars = Integer.valueOf(rdButton.getText().toString());
+
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                long inserted_id = dbh.insertSong(title,singer,year, stars);
+                dbh.close();
+
+                if (inserted_id != -1){
+                    Toast.makeText(MainActivity.this, "Insert successful",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent i = new Intent(MainActivity.this,
+                        SongArrayAdapter.class);
+
+                startActivityForResult(i,9);
+            }
+        });
+
     }
 }

e68463c5838784e6d48997ecf213c76322ac8d2d
julien <julienngo911@gmail.com>
2021-05-18 13:29:50 +0800

    First Commit Julien (Added row.xml, SongArrayAdapter.java)

---
+++ app/src/main/java/com/myapplicationdev/android/p05_ndpsongs/SongArrayAdapter.java
@@ -0,0 +1,70 @@
+package com.myapplicationdev.android.p05_ndpsongs;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class SongArrayAdapter extends ArrayAdapter<Song> {
+    Context context;
+    ArrayList<Song> songs;
+    int resource;
+    TextView title, singer, year;
+
+    public SongArrayAdapter(Context context, int resource, ArrayList<Song> songs) {
+        super(context, resource, songs);
+        this.context = context;
+        this.songs = songs;
+        this.resource = resource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(resource, parent, false);
+        title = (TextView) rowView.findViewById(R.id.tvSongNameRow);
+        singer = (TextView) rowView.findViewById(R.id.tvSingerRow);
+        year = (TextView) rowView.findViewById(R.id.tvYearRow);
+        ImageView iv1 = (ImageView) rowView.findViewById(R.id.imageView1star);
+        ImageView iv2 = (ImageView) rowView.findViewById(R.id.imageView2star);
+        ImageView iv3 = (ImageView) rowView.findViewById(R.id.imageView3star);
+        ImageView iv4 = (ImageView) rowView.findViewById(R.id.imageView4star);
+        ImageView iv5 = (ImageView) rowView.findViewById(R.id.imageView5star);
+
+        Song song = songs.get(position);
+        year.setText(song.getYears());
+        title.setText(song.getTitle());
+        singer.setText(song.getSingers());
+
+        //Check if the property for starts == 5, if so, "light" up the stars
+        if (song.getStars() == 5) {
+            iv5.setImageResource(android.R.drawable.btn_star_big_on);
+            iv4.setImageResource(android.R.drawable.btn_star_big_on);
+            iv3.setImageResource(android.R.drawable.btn_star_big_on);
+            iv2.setImageResource(android.R.drawable.btn_star_big_on);
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        } else if (song.getStars() == 4) {
+            iv4.setImageResource(android.R.drawable.btn_star_big_on);
+            iv3.setImageResource(android.R.drawable.btn_star_big_on);
+            iv2.setImageResource(android.R.drawable.btn_star_big_on);
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        } else if (song.getStars() == 3) {
+            iv3.setImageResource(android.R.drawable.btn_star_big_on);
+            iv2.setImageResource(android.R.drawable.btn_star_big_on);
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        } else if (song.getStars() == 2) {
+            iv2.setImageResource(android.R.drawable.btn_star_big_on);
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        } else {
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        }
+
+        return rowView;
+    }
+}
